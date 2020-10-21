b3baa9c50d5594cd159390143aa250416b3a9b63
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-10-21 19:05:00 +0200

    3rd point

--- paridispari/main.js
+++ paridispari/main.js
@@ -2,25 +2,35 @@
 
 
 //creo una funzione per definire se pari o dispari
-function isItEven(number) {
-    if (number % 2 == 0){
-        return true;
-    } else {
-        return false;
-    }
+// function isItEven(number) {
+//     if (number % 2 == 0){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+function isEven(num) {
+    return num % 2 == 0;
 }
 
-//creo una variabile per il numero inserito dall'utente
-var insertedNumber;
+//chiedo all' utente un numero da 1 a 5
+var selectedNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+//creo un ciclo con la condizione che l'utente deve inseire un numero valido, maggiore di 0 e minore di 5
+while (selectedNumber <= 0 || selectedNumber > 5 || isNaN(selectedNumber)) {
+    selectedNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+}
+console.log("your number is " + selectedNumber);
 
-var numberOfGames = 3;
-var contatore = 0;
-//creo ciclo dove l'utente inserisci un numero da 1 a 5
-do {
-    insertedNumber = prompt("Inserisci un numero da 1 a 5");
-    console.log(insertedNumber);
-    if ( !isNaN(insertedNumber) && insertedNumber >= 1 && insertedNumber <= 5) {
-        var playerNumber = isItEven(insertedNumber);
-    } 
-    contatore++;
-} while (numberOfGames < contatore);
+var selection  = ["pari", "dispari"]
+
+//chiedo all'utente se l'esito fra i due numeri  sarà pari o dispari
+var esitoTotale = prompt("pari o dispari?");
+while (!selection.includes(esitoTotale)) {
+    esitoTotale = prompt("pari o dispari?");
+}
+console.log("you choose " + esitoTotale);
+
+//genero un numero per il bot
+var botNumber = Math.floor((Math.random() * 5) + 1);
+console.log("bot's number is : " + botNumber);
